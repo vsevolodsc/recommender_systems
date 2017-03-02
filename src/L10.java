c3871063623ab593a9d6150427636780da46d544
Vsevolods Caka <vsevolods.c@gmail.com>
2017-03-02 13:20:53 +0000

    CSV writer implemented

--- src/L10.java
+++ src/L10.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.List;
 
@@ -5,7 +8,7 @@ import java.util.List;
  * Created by vsevo on 23/02/2017.
  */
 public class L10 {
-
+    private List<String> out_to_csv;
     public double rmse(List<Integer> predicted, List<Integer> actual){
         if(predicted.size()!=actual.size()){
             return 0;
@@ -19,6 +22,12 @@ public class L10 {
 
     public void leave_one_out(Populate pop){
         HashMap<Integer, HashMap<Integer, Integer>> users = pop.getUsers();
-        
+        StringBuilder sb = new StringBuilder();
+
+    }
+
+    public void out_to_csv() throws FileNotFoundException{
+        PrintWriter pw = new PrintWriter(new File("./L10.csv"));
+
     }
 }
